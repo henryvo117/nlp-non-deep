3599f2c410e5a76b8c92c014a914b8af8e60b8d4
Luu Hoang Long VO <luu-hoang-long.vo@epita.fr>
2022-10-06 21:24:11 +0200

    refactorizing

---
+++ lab_04/bilstm.py
@@ -0,0 +1,64 @@
+import torch.nn as nn
+import torch
+
+class BiLSTM(nn.Module):
+    """A simple RNN module with word embeddings.
+    """
+    def __init__(self, vocab_size: int, embed_size: int, hidden_size: int, n_layers: int, n_outputs: int, device: torch.device) -> None:
+        """
+        Args:
+            vocab_size: vocabulary size.
+            embed_size: embedding dimensions.
+            hidden_size: hidden layer size.
+            n_layers: the number of layers.
+            n_outputs: the number of output classes.
+            device: the device is being used
+        """
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.n_outputs = n_outputs
+        self.device = device
+
+        # The word embedding layer.
+        self.embed = nn.Embedding(self.vocab_size, self.embed_size)
+        # The RNN
+        self.lstm = nn.LSTM(
+            input_size = self.embed_size,
+            hidden_size = self.hidden_size,
+            num_layers = self.n_layers,
+            batch_first = True, # Changes the order of dimension to put the batches first.
+            bidirectional=True
+        )
+        # A fully connected layer to project the LSTM's output to only one output used for classification.
+        self.fc = nn.Linear(self.hidden_size * 2, self.n_outputs)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Function called when the model is called with data as input.
+        Args:
+            X: the input tensor of dimensions batch_size, sequence length, vocab size (actually just an int).
+        Returns:
+            The resulting tensor of dimension batch_size, sequence length, output dimensions.
+        """
+        h0 = (torch.zeros(self.n_layers * 2, X.size(0), self.hidden_size).to(self.device), torch.zeros(self.n_layers * 2, X.size(0), self.hidden_size).to(self.device))
+        out = self.embed(X)
+        # out contains the output layer of all words in the sequence.
+        # First dim is batch, second the word in the sequence, third is the vector itself.
+        # The second output value is the last vector of all intermediate layer.
+        # Only use it if you want to access the intermediate layer values of a
+        # multilayer model.
+        out, _ = self.lstm(out, h0)
+        # Getting the last value only.
+        #out = out[:, -1, :]
+        out = torch.cat((out[:, -1, :self.hidden_size].squeeze(1), out[:, 0, self.hidden_size:].squeeze(1)), dim=1)
+        
+        # Tried max-pool and also addition
+        # out = out[:, -1, :self.hidden_size].squeeze(1) + out[:, 0, self.hidden_size:].squeeze(1)
+        # out = torch.maximum(out[:, -1, :self.hidden_size].squeeze(1), out[:, 0, self.hidden_size:].squeeze(1))
+        
+        # Linear projection.
+        out = self.fc(out)
+
+        return out
